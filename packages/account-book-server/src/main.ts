14132a1f587dc802e0cff1a6c9a84bba13c1fb7b
HyungwonJin <hwjin92@gmail.com>
2023-11-27 09:41:50 +0900

    feat: Swagger setting

--- packages/account-book-server/src/main.ts
+++ packages/account-book-server/src/main.ts
@@ -1,4 +1,8 @@
 import Fastify from 'fastify'
+import {swaggerUiConfig} from "./config/swaggerUiConfig";
+import {swaggerConfig} from "./config/swagger";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const server = Fastify({ logger: true })
 
@@ -7,3 +11,6 @@ server.get('/ping', async (request, reply) => {
 })
 
 server.listen({ port: 4000 })
+
+await server.register(fastifySwagger, swaggerConfig)
+await server.register(fastifySwaggerUi, swaggerUiConfig)
