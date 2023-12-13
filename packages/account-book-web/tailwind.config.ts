4c9b45605179a0cb0ab38a112012c15d64db2a37
HyungwonJin <hwjin92@gmail.com>
2023-12-13 16:14:17 +0900

    chore: color setting

--- packages/account-book-web/tailwind.config.ts
+++ packages/account-book-web/tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      gray: {
+        0: "#ECECEC",
+        1: "#C6C6C6",
+        2: "#A0A0A0",
+        3: "#4B4B4B",
+        4: "#2E2E2E",
+        5: "#121212",
       },
+      primary: "#C9DB03",
+      secondary: "#FEF300",
+      point: "#81A003",
+      sub: "#FBF1B2",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
