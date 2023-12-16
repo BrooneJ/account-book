83dc245f64edae36f8e505f7ee5f21b71006ae97
HyungwonJin <hwjin92@gmail.com>
2023-12-16 19:52:07 +0900

    feat: implements register page

--- packages/account-book-web/src/app/(beforLogin)/layout.tsx
+++ packages/account-book-web/src/app/(beforLogin)/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="px-5 pb-[20px] h-screen">{children}</div>;
+  return <div className="px-5 pb-[20px] h-full">{children}</div>;
 }
--- packages/account-book-web/src/app/(beforLogin)/register/page.tsx
+++ packages/account-book-web/src/app/(beforLogin)/register/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import { useGoBack } from "@/hooks/useGoBack";
+import Header from "@/app/ui/Header/Header";
+import HeaderBackButton from "@/app/ui/Header/HeaderBackButton";
+import { Input } from "@/app/ui/loginRegister/Input";
+import Link from "next/link";
+import { Button } from "@/app/ui/loginRegister/Button";
+
 export default function Register() {
-  return <div>register</div>;
+  const onClick = useGoBack();
+  return (
+    <div className="flex flex-col h-full justify-between">
+      <div>
+        <Header headerLeft={<HeaderBackButton onClick={onClick} />} />
+        <div className="text-2xl pl-1 pt-10 font-semibold">
+          <span className="font-extrabold text-4xl">登録</span>して
+          <br />
+          サービスを楽しみましょう！
+        </div>
+        <div className="pt-[50px] h-56 flex flex-col justify-between">
+          <Input placeholder="メールアドレス" />
+          <Input placeholder="ユーザー名" />
+          <Input placeholder="パスワード" />
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <span className="text-[19px] pb-2">
+          アカウントをお持ちの方は
+          <Link href="/login">
+            <span className="text-point font-bold">ログイン</span>
+          </Link>
+        </span>
+        <Button layoutMode="fullWidth">登録</Button>
+      </div>
+    </div>
+  );
 }
