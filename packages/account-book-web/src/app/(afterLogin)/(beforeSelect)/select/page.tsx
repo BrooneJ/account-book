2e9973354b8eff630f8203e5d88f7377296b6450
HyungwonJin <hwjin92@gmail.com>
2024-01-16 10:24:29 +0900

    fix: Redirect to login page upon token expiration logout

--- packages/account-book-web/src/app/(afterLogin)/(beforeSelect)/select/page.tsx
+++ packages/account-book-web/src/app/(afterLogin)/(beforeSelect)/select/page.tsx
@@ -6,9 +6,13 @@ import {
 } from "@tanstack/react-query";
 import { getAccount } from "@/app/lib/getAccount";
 import HomeComponent from "@/app/ui/Home/HomeComponent";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const me = await getMyAccount();
+  if (me.statusCode) {
+    return redirect("/login");
+  }
 
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
