bc04a9e4cb15b68b1e3e23f2cccfa5d9cb9ff687
HyungwonJin <hwjin92@gmail.com>
2024-01-30 10:18:47 +0900

    fix: Reset category and source data when returning from transaction detail to list

--- packages/account-book-web/src/app/ui/Common/CloseButton.tsx
+++ packages/account-book-web/src/app/ui/Common/CloseButton.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { useGoBack } from "@/app/hooks/useGoBack";
+import { categorySourceStore } from "@/app/store/categorySourceStore";
 
 export default function CloseButton() {
   const goBack = useGoBack();
+  const { setCategory, setSource } = categorySourceStore();
   return (
     <div
       className="bg-background rounded-xl"
       onClick={() => {
         goBack();
+        setCategory({ type: "expense", name: "未登録" });
+        setSource({ type: "expense", name: "未登録" });
       }}
     >
       <Image src="/images/close.svg" alt="close" width={20} height={20} />
