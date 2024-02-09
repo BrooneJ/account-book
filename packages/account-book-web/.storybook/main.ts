ae20e0deea801869ac0ce4746470c5be4b5946f3
HyungwonJin <hwjin92@gmail.com>
2024-02-09 15:06:19 +0900

    feat: add MonthIndicator.stories.ts

---
+++ packages/account-book-web/.storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+// @ts-ignore
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src/");
+    return config;
+  },
+  features: {
+    experimentalRSC: true,
+  },
+};
+export default config;
--- packages/account-book-web/src/app/ui/Statistics/StatisticsPeriodSelector/StatisticsPeriodSelector.stories.ts
+++ packages/account-book-web/src/app/ui/Statistics/StatisticsPeriodSelector/StatisticsPeriodSelector.stories.ts
@@ -1,4 +1,4 @@
-import StatisticsPeriodSelector from "@/app/ui/Statistics/StatisticsPeriodSelector";
+import StatisticsPeriodSelector from "@/app/ui/Statistics/StatisticsPeriodSelector/index";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
